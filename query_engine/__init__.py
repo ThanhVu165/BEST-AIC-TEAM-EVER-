e1346abcb195c597ba5aafd01b6f77f7c369342e
Thành Vũ <Ndthanhvu@gmail.com>
2026-08-16 19:20:50 +0700

    feat(query): expose baseline engine

--- query_engine/__init__.py
+++ query_engine/__init__.py
@@ -1,5 +1,6 @@
 """Query Engine package."""
 
+from .engine import BaselineQueryEngine
 from .mock_engine import MockQueryEngine
 
-__all__ = ["MockQueryEngine"]
+__all__ = ["BaselineQueryEngine", "MockQueryEngine"]
