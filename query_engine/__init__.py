d19e1721ff57200f11041bac9202a7e050b94340
Thành Vũ <Ndthanhvu@gmail.com>
2026-08-17 17:15:01 +0700

    feat(query): export semantic reranker interfaces

--- query_engine/__init__.py
+++ query_engine/__init__.py
@@ -6,6 +6,12 @@ from .engine import BaselineQueryEngine
 from .mock_engine import MockQueryEngine
 from .query_understanding import QueryEventSpec, QuerySpec, understand_query
 from .semantic import SemanticQuery, decompose_query, semantic_score
+from .semantic_reranker import (
+    SemanticRerankConfig,
+    SigLIP2ImageTextScorer,
+    build_semantic_reranker,
+    semantic_text,
+)
 from .vlm_answering import TransformersImageAnswerExtractor
 
 __all__ = [
@@ -17,9 +23,13 @@ __all__ = [
     "QueryEventSpec",
     "QuerySpec",
     "SemanticQuery",
+    "SemanticRerankConfig",
+    "SigLIP2ImageTextScorer",
     "TransformersImageAnswerExtractor",
     "UnavailableAnswerExtractor",
+    "build_semantic_reranker",
     "decompose_query",
     "semantic_score",
+    "semantic_text",
     "understand_query",
 ]
